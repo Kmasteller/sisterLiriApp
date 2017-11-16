7142272c231f60ac932764b7533a6ca26ff8d972
Kmasteller <kennethmasteller@gmail.com>
2017-11-15 21:08:15 -0700

    roundTwo

---
+++ liri.js
@@ -0,0 +1,7 @@
+var myTwitterKeys = require("./keys.js");
+
+console.log("--------------------------");
+console.log(myTwitterKeys);
+console.log("--------------------------");
+
+var recentTweet = process.argv[2];
