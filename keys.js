a64025924d659a520d4b22c8ac625091b8aebf30
Kmasteller <kennethmasteller@gmail.com>
2017-11-18 14:30:22 -0700

    take three

--- keys.js
+++ keys.js
@@ -3,7 +3,7 @@ console.log('this is loaded');
 var twitterKeys = {
   consumer_key: '	e44oMmzkRXGSLZ2TjBu58BgHy',
   consumer_secret: 'ys1qh42URxr9TYIiRyE4GPIqHl8R9DLONN54Je57PfXGoLcHkp',
-  access_token_key: '	930998308402733057-Eci54hvW964a2EJO9yQNaQDwjsSUWp3',
+  access_token_key: '930998308402733057-Eci54hvW964a2EJO9yQNaQDwjsSUWp3',
   access_token_secret: 'QLoN2fJSWrXoVbdcp8ig7fZRjck5OuGEva4h54mBvD03Q'
 }
 
--- liri.js
+++ liri.js
@@ -1,7 +1,53 @@
-var myTwitterKeys = require("./keys.js");
+var tKeys = require("./keys.js");
 
 console.log("--------------------------");
-console.log(myTwitterKeys);
+console.log(tKeys);
 console.log("--------------------------");
 
-var recentTweet = process.argv[2];
+var userInput = process.argv[2];
+
+var Spotify = require("node-spotify-api");
+
+var request = require("request");
+
+// My-Tweets
+switch (userInput) {
+    case "my-tweets":
+        getTwitter(tKeys);
+        break;
+    case "spotify-this-song":
+        getSpotify();
+        break;
+    case "movie-this":
+        getOMDB();
+        break;
+    case "do-what-it-says":
+        getCommand();
+        break;
+    default: 
+
+}
+
+
+  });
+
+function getTwitter(tKeys) {
+
+    var Twitter = require("twitter");
+
+    var client = new Twitter({
+        consumer_key: tKeys.consumer_key,
+        consumer_secret: tKeys.consumer_secret,
+        access_token_key: tKeys.access_token_key,
+        access_token_secret: tKeys.access_token_secret
+    });
+
+client.get("statuses/user_timeline", function (error, tweets, response) {
+    if (!error) {
+        console.log(tweets);
+    }
+
+    tweets.forEach(function(t) {
+        console.log(t);
+    });
+};
