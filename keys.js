83958967cc0cabe033ab27262fa7da13d1a92244
Kmasteller <kennethmasteller@gmail.com>
2017-11-26 20:01:00 -0700

    was working

--- keys.js
+++ keys.js
@@ -1,10 +1,18 @@
 // console.log('this is loaded');
 
-var tKeys = {
-  consumer_key: 'qS0LOvOVf4hSR6TafA6W7bOhR',
-  consumer_secret: '92cL34QQLchrRdRfQEbVG0Bqcosd8CNun7nCqA4zAVCcAVoIiG',
-  access_token_key: '930998308402733057-Eci54hvW964a2EJO9yQNaQDwjsSUWp3',
-  access_token_secret: 'QLoN2fJSWrXoVbdcp8ig7fZRjck5OuGEva4h54mBvD03Q'
+var twitterKeys = {
+  consumer_key: 'ndymRXkvGuF1b7nQtlhC9kfZU',
+  consumer_secret: 'wgPLDrG5zrew5X0znu7SEfwCIHhYUgPUsqNeByA6QColqoOcoI',
+  access_token_key: '930998308402733057-dSLJ7YUsKCUxnbbadcCnCx8CIdPVPBd',
+  access_token_secret: 'vqeEuTVsECqGPaUVobCfghGKJ9MLtO4sQUsJGaFSqGLjK'
 }
 
-module.exports = tKeys;
+module.exports = twitterKeys;
+
+var spotifyKeys= {
+  client_id: "a9dbc229eff84585ad9a3f32c80ba3e7"
+}
+
+module.exports = spotifyKeys;
+
+console.log("Link Check");
--- liri.js
+++ liri.js
@@ -1,51 +1,112 @@
-var tKeys = require("./keys.js");
+var keys = require("./keys.js");
 
-console.log(tKeys);
+console.log(keys);
 console.log("--------------------------");
 
-var userInput = process.argv[2];
+var request = require('request');
 
-var Spotify = require("node-spotify-api");
+var Twitter = require("twitter");
 
-var request = require("request");
+var spotify = require("spotify");
 
-// My-Tweets
-switch (userInput) {
-    case "my-tweets":
-        getTwitter(tKeys);
-        break;
-    case "spotify-this-song":
-        getSpotify();
-        break;
-    case "movie-this":
-        getOMDB();
-        break;
-    case "do-what-it-says":
-        doCommand();
-        break;
-    default: 
+var getTwitter = function() {
 
-}
+    var client = new Twitter(keys);
 
-function getTwitter(tKeys) {
+    // console.log(client);
 
-    var Twitter = require("twitter");
-
-    var client = new Twitter({
-        consumer_key: tKeys.consumer_key,
-        consumer_secret: tKeys.consumer_secret,
-        access_token_key: tKeys.access_token_key,
-        access_token_secret: tKeys.access_token_secret
+    var params = {screen_name: "Kenny Quinn, Esq."};
+            
+    client.get("statuses/user_timeline", params, function(error, tweets, response) {
+                
+        if (!error) {
+            for(var i=0; i<tweets.length; i++) {
+                console.log(tweets[i].created_at);
+                console.log(tweets[i].text);
+                console.log("=================");
+            }} else {
+                console.log(error);
+        }
     });
 }
 
-client.get("statuses/user_timeline", function (error, tweets, response) {
+var artistName = function(artist) {
+    return artist.name;
+}
 
-    if (!error) {
-        console.log(tweets);
+var getSpotify = function(songName) {
+
+    spotify.search({ type: "track", query: songName}, function(err, data) {
+        if ( err ) {
+            console.log(err);
+            return;
+        }
+
+        // var songs = data.tracks.items;
+        // for(var i=0; i<songs.length; i++) {
+        //     console.log[i];
+        //     console.log("Artist(s): " + songs[i].artists.map(artistName));
+        //     console.log("Song name: " + songs[i].preview_url);
+        //     console.log("Album: " + songs[i].album.name);
+        //     console.log("=========================")
+        // }
+        console.log(data); 
+    });
+}
+
+var getMovie = function(movieName) {
+    request("http://www.omdbapi.com/?t=pulp+fiction&y=&plot=short&r=json", function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+            var jsonData = JSON.parse(body);
+            console.log("Title: " + jsonData.Title);
+            console.log("year: " + jsonData.Year);
+            console.log("Rated: " + jsonData.Rated);
+            console.log("IMDB Rating: " + jsonData.imdbRating);
+            console.log("Country: " + jsonData.Country);
+            console.log("Language: " + jsonData.Language);
+            console.log("Plot: " + jsonData.Plot);
+            console.log("Actors: " + jsonData.Actors);
+            console.log("Rotten Tom Score: " + jsonData.tomatoRating);
+        } 
+        console.log(error);
+    })
+}
+
+var doCommand = function() {
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (err) throw err;
+        
+        var dataArr = data.split(", ");
+        
+        if (dataArr.length == 2) {
+            pick(dataArr[0], dataArr[1]);
+        } else if (dataArr.length == 1) {
+            prick(dataArr[0]);
+        }
+    });
+}
+
+var select = function(caseData, functionData) {
+    switch(caseData) {
+        case "my-tweets":
+            getTwitter();
+            break;
+        case "spotify-this-song":
+            getSpotify(functionData);
+            break;
+        case "movie-this":
+            getMovie(functionData);
+            break;
+        case "do-what-it-says":
+            doCommand();
+            break;
+        default:
+            console.log("Liri is not that smart, please try again!");
     }
+}
 
-    tweets.forEach(function(t) {
-        console.log(t);
-    });
-})
+var runThis = function(argOne, argTwo) {
+    select(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
