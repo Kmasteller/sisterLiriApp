17defab8e9a8d3d72a6e573713c6120e95ffe400
Kmasteller <kennethmasteller@gmail.com>
2017-11-30 18:26:53 -0700

    hiding keys

--- keys.js
+++ keys.js
@@ -1,9 +1,4 @@
-var twitterKeys = {
-  consumer_key: 'ndymRXkvGuF1b7nQtlhC9kfZU',
-  consumer_secret: 'wgPLDrG5zrew5X0znu7SEfwCIHhYUgPUsqNeByA6QColqoOcoI',
-  access_token_key: '930998308402733057-dSLJ7YUsKCUxnbbadcCnCx8CIdPVPBd',
-  access_token_secret: 'vqeEuTVsECqGPaUVobCfghGKJ9MLtO4sQUsJGaFSqGLjK'
-}
+
 
 module.exports = twitterKeys;
 
